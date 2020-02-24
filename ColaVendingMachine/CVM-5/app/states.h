5bba6045a8c7b7abf9c437ef95c5b15006e2696b
jos <jos.onokiewicz@han.nl>
2020-02-24 14:25:51 +0100

    Update events and states

--- ColaVendingMachine/CVM-5/app/states.h
+++ ColaVendingMachine/CVM-5/app/states.h
@@ -2,12 +2,12 @@
 #define STATES_H
 
 typedef enum {
-   S_NO,
-   S_START,
-   S_INITIALISED,
+   S_NO,    ///< Used for initialisation
+   S_START, ///< Initial state
+   S_INITIALISED_SUBSYSTEMS,
    S_WAIT_FOR_COINS,
-   S_DETECTED_10C,
    S_DETECTED_20C,
+   S_DETECTED_50C,
    S_DISPENSE,
    S_WAIT_FOR_UPDATE_CHANGE
 } state_e;
