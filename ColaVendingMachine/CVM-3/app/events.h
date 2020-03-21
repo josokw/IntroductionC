a888157019c37fc0bba7f54b7943ee157478f304
jos <jos.onokiewicz@han.nl>
2020-03-21 17:37:15 +0100

    Add more documentation

--- ColaVendingMachine/CVM-3/app/events.h
+++ ColaVendingMachine/CVM-3/app/events.h
@@ -2,15 +2,15 @@
 #define EVENTS_H
 
 typedef enum {
-   E_NO, ///< Used for initialisation of an event variable.
-   E_START, ///< first event after CVM is switched on.
-   E_CONTINUE, ///< Initialising subsystems is ready.
-   E_20C, ///< 20C coin.
-   E_50C, ///< 50C coin.
-   E_NOT_ENOUGH, 
-   E_ENOUGH,
-   E_CHANGE_DISPENSE,
-   E_NO_CHANGE_DISPENSE
+   E_NO,                ///< Used for initialisation of an event variable.
+   E_START,             ///< first event after CVM is switched on.
+   E_CONTINUE,          ///< Initialising subsystems is ready.
+   E_20C,               ///< 20C coin.
+   E_50C,               ///< 50C coin.
+   E_NOT_ENOUGH,        ///< Not enough coins inserted
+   E_ENOUGH,            ///< Enough coins inserted
+   E_CHANGE_DISPENSE,   ///< Dispense of change possible
+   E_NO_CHANGE_DISPENSE ///< No dispense of change possible
 } event_e;
 
 #endif
