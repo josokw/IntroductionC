90566ee882cafa7535bfceeeced2cf6f3d80518d
jos <jos.onokiewicz@han.nl>
2020-03-22 15:07:53 +0100

    Add documentation comments

--- ColaVendingMachine/CVM-3/app/states.h
+++ ColaVendingMachine/CVM-3/app/states.h
@@ -2,14 +2,14 @@
 #define STATES_H
 
 typedef enum {
-   S_NO,    ///< Used for initialisation
+   S_NO,    ///< Used for initialisation if state is not yet known
    S_START, ///< Initial state
-   S_INITIALISED_SUBSYSTEMS,
-   S_WAIT_FOR_COINS,
-   S_DETECTED_20C,
-   S_DETECTED_50C,
-   S_DISPENSE,
-   S_WAIT_FOR_UPDATE_CHANGE
+   S_INITIALISED_SUBSYSTEMS, ///< All subsystems initialised
+   S_WAIT_FOR_COINS,         ///< Wait for coin inserts
+   S_DETECTED_20C,           ///< Detected 20 cents coin insert
+   S_DETECTED_50C,           ///< Detected 50 cents coin insert
+   S_DISPENSE,               ///< Checks if change is available
+   S_WAIT_FOR_UPDATE_CHANGE  ///< Wait for update change (simulation)
 } state_e;
 
 #endif
